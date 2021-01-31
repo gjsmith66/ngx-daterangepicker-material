b7823811f7c1a29fdc426ab33daccbeff598345d
Gary Smith <gjsmith66@gmail.com>
2021-01-31 11:40:18 -0500

    Auto-generated commit

--- src/daterangepicker/index.js
+++
@@ -1,5 +0,0 @@
-export { NgxDaterangepickerMd } from './daterangepicker.module';
-export { DaterangepickerComponent } from './daterangepicker.component';
-export { DaterangepickerDirective } from './daterangepicker.directive';
-export { LOCALE_CONFIG, DefaultLocaleConfig } from './daterangepicker.config';
-export { LocaleService } from './locale.service';
